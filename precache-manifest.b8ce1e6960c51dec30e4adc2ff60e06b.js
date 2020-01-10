3af6bee8a51f355aa66aa5fd6d5b22d975199f9c
Vachik <amiryanvachik@gmail.com>
2020-01-10 18:39:13 +0400

    Updates

---
+++ precache-manifest.b8ce1e6960c51dec30e4adc2ff60e06b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f901d22ccc192ec835a8c2b4afe5cb",
+    "url": "/blogpost-react/index.html"
+  },
+  {
+    "revision": "171d4030b239fc2a78be",
+    "url": "/blogpost-react/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "866934de608322e63286",
+    "url": "/blogpost-react/static/css/main.e0ee55d0.chunk.css"
+  },
+  {
+    "revision": "171d4030b239fc2a78be",
+    "url": "/blogpost-react/static/js/2.afa09e50.chunk.js"
+  },
+  {
+    "revision": "866934de608322e63286",
+    "url": "/blogpost-react/static/js/main.2d736f79.chunk.js"
+  },
+  {
+    "revision": "7b7f9645a795738bab84",
+    "url": "/blogpost-react/static/js/runtime-main.f3ec9d32.js"
+  }
+]);
